f2015c67456b683091fef452e856c5a0ce5a6265
vifcoder <[shurchandata@gmail.com]>
2015-01-31 17:39:05 -0500

    ttt

---
+++ test1.py
@@ -0,0 +1,19 @@
+X = 99
+
+def func():
+    X = 88
+
+print func()
+
+# Global scope
+X = 99                # X and func assigned in module: global
+          
+def func(Y):          # Y and Z assigned in function: locals
+    # local scope
+    Z = X + Y         # X is a global.
+    return Z
+
+
+print func(1)               # func in module: result=100
+print func(3)
+
---
+++ test2.py
@@ -0,0 +1,2 @@
+print 2**8
+print 'test string1' + 'test string2'
