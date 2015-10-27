9a09e8269f722156959661eeb41f4af9d64abb4c
Jeren <jerenchang@gmail.com>
2015-10-26 23:31:29 -0400

    dd mysql-install.sh

--- mysql-install.sh
+++ mysql-install.sh
@@ -9,3 +9,4 @@ sudo apt-get -y install mysql-server
 sed -i "s/^bind-address/#bind-address/" /etc/mysql/my.cnf
 mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 sudo /etc/init.d/mysql restart
+s
